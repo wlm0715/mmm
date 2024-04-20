1fc26023fc946c4c6b29f341c11d78a051bc749c
3366903063 <3366903063@qq.com>
2024-04-20 13:42:28 +0800

    gitee

--- src/wlm.java
+++ src/wlm.java
@@ -1,5 +1,8 @@
 public class wlm {
     public  void main(String[] args) {
-        System.out.println("Hello GitHub");
+
+        System.out.println("姓名：温联民 ");
+        System.out.println("班级：22物联网03");
+        System.out.println("学号：2022133381");
     }
 }
