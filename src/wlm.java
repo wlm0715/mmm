f62b55ab3095a27eb6246f132955bc2394f474c1
3366903063 <3366903063@qq.com>
2024-04-30 14:53:59 +0800

    asd

--- src/wlm.java
+++ src/wlm.java
@@ -1,8 +1,7 @@
 public class wlm {
-    public  void main(String[] args) {
-
-        System.out.println("姓名：温联民 ");
-        System.out.println("班级：22物联网03");
-        System.out.println("学号：2022133381");
+    public static void main(String[] args) {
+        System.out.println("姓名：");
+        System.out.println("班级：");
+        System.out.println("学号：");
     }
 }
